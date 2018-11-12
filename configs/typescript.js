1b8793de9d9385b77ced81ab4b7680c7aeddd428
Rolf Erik Lekang <me@rolflekang.com>
2018-11-12 09:16:20 +0100

    fix: Use typescript version of no-use-before-define in ts

--- configs/typescript.js
+++ configs/typescript.js
@@ -4,6 +4,9 @@ module.exports = {
     'no-unused-vars': 0,
     'typescript/no-unused-vars': 2,
 
+    'no-use-before-define': 0,
+    'typescript/no-use-before-define': 2,
+
     'typescript/no-explicit-any': 1,
     'typescript/explicit-function-return-type': [0, { allowExpressions: true }],
   },
