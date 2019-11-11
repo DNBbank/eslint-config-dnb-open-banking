e2af4e09bbcfa11eeb958add93c31de2138afe3e
Rolf Erik Lekang <rolf.erik.heggem.lekang@dnb.no>
2019-11-11 12:53:35 +0100

    fix: Disable node/no-unsupported-features/es-syntax

--- configs/node.js
+++ configs/node.js
@@ -7,5 +7,9 @@ module.exports = {
   rules: {
     'node/prefer-promises/fs': 2,
     'node/prefer-promises/dns': 2,
+
+    // We use either typescript or babel for most projects so
+    // this can be activated per project if necessary
+    'node/no-unsupported-features/es-syntax': 0,
   },
 };
