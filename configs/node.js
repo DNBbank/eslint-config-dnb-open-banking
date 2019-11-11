b2499b8b591dc973e2ce1b923836a320e9598313
Rolf Erik Lekang <rolf.erik.heggem.lekang@dnb.no>
2019-11-11 13:05:04 +0100

    fix: Disable node/no-missing-import

--- configs/node.js
+++ configs/node.js
@@ -11,5 +11,8 @@ module.exports = {
     // We use either typescript or babel for most projects so
     // this can be activated per project if necessary
     'node/no-unsupported-features/es-syntax': 0,
+
+    // This is handled by the import plugin in the main config
+    'node/no-missing-import': 0,
   },
 };
